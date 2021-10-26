d32b35ab8a1fa881bcd6386f6b2fd0f8aa061fc6
Jus de Patate <jusdepatate@protonmail.com>
2021-10-27 01:36:39 +0200

    add `method` argument to raw.php

--- raw.php
+++ raw.php
@@ -1,10 +1,12 @@
 <?php
-# same as index.php but with raw output (no other bullshit than ip/host/ua)
+# same as index.php but with raw output (no other bullshit than ip/host/ua/method)
 
 if (isset($_GET['host'])) {
     echo gethostbyaddr($_SERVER['REMOTE_ADDR'])."\n";
 } elseif (isset($_GET['ua'])) {
     echo $_SERVER['HTTP_USER_AGENT']."\n";
+} elseif (isset($_GET['method'])) {
+    echo $_SERVER['REQUEST_METHOD']."\n";
 } else {
     echo $_SERVER['REMOTE_ADDR']."\n";
 }
